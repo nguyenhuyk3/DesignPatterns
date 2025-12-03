24afdcd76a518eb7f8ec5783f164b2634f86df8e
nguyenhuyk3 <huykimcuong5@gmail.com>
2025-12-03 15:06:58 +0700

    Add Singleton pattern and usage example

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DesignPattern.Creational;
+using System;
 
 namespace DesignPattern
 {
@@ -10,6 +7,18 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
         }
     }
 }
